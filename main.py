8d3f900a124b29cc10e7580c2ef0488b5dca763b
Samuel Tonial <lokosamuca23@gmail.com>
2023-05-28 00:24:35 -0300

    English Traduction

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ def gerar_qrcode():
     pasta_destino = entry_pasta_destino.get()
 
     if not pasta_excel or not nome_planilha or not nome_coluna_link or not nome_coluna_texto or not pasta_destino:
-        messagebox.showwarning("Aviso", "Por favor, preencha todos os campos.")
+        messagebox.showwarning("warning", "Please fill in all fields.")
         return
 
     if not os.path.isfile(pasta_excel):
-        messagebox.showerror("Erro", "Arquivo Excel não encontrado.")
+        messagebox.showerror("Error", "Excel file not found.")
         return
 
     try:
@@ -62,49 +62,49 @@ def gerar_qrcode():
             qrcodes_gerados.append(nome_arquivo_gerado)
 
         if qrcodes_gerados:
-            messagebox.showinfo("Sucesso", "QR codes gerados com sucesso!")
+            messagebox.showinfo("Success", "QR codes successfully generated!")
     except Exception as e:
-        messagebox.showerror("Erro", str(e))
+        messagebox.showerror("Error", str(e))
         return
 
 janela = Tk()
-janela.title("Gerador de QR Codes")
+janela.title("QR Code Generator")
 janela.geometry("400x500")
 janela.configure(bg="#D31145")
 
-label_pasta_excel = Label(janela, text="Pasta do Excel:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
+label_pasta_excel = Label(janela, text="Excel file:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
 label_pasta_excel.pack(pady=(10,0))
 entry_pasta_excel = Entry(janela)
 entry_pasta_excel.pack()
-button_selecionar_pasta_excel = Button(janela, text="Selecionar Pasta", font=("KelloggsSansMedium", 10,"bold"), command=selecionar_pasta_excel)
+button_selecionar_pasta_excel = Button(janela, text="Select Folder", font=("KelloggsSansMedium", 10,"bold"), command=selecionar_pasta_excel)
 button_selecionar_pasta_excel.pack(pady=5)
 
-label_nome_planilha = Label(janela, text="Nome da Planilha:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
+label_nome_planilha = Label(janela, text="Sheet Name:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
 label_nome_planilha.pack(pady=(20,0))
 entry_nome_planilha = Entry(janela)
 entry_nome_planilha.pack()
 
-label_nome_coluna_link = Label(janela, text="Coluna do Link:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
+label_nome_coluna_link = Label(janela, text="Column with the Links:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
 label_nome_coluna_link.pack(pady=(20,0))
 entry_nome_coluna_link = Entry(janela)
 entry_nome_coluna_link.pack()
 
-label_nome_coluna_texto = Label(janela, text="Coluna do Texto:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
+label_nome_coluna_texto = Label(janela, text="Text Column:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
 label_nome_coluna_texto.pack(pady=(20,0))
 entry_nome_coluna_texto = Entry(janela)
 entry_nome_coluna_texto.pack()
 
-label_pasta_destino = Label(janela, text="Pasta de Destino:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
+label_pasta_destino = Label(janela, text="Destination Folder:", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 15,"bold"))
 label_pasta_destino.pack(pady=(20,0))
 entry_pasta_destino = Entry(janela)
 entry_pasta_destino.pack()
-button_selecionar_pasta_destino = Button(janela, text="Selecionar Pasta", font=("KelloggsSansMedium", 10,"bold"), command=selecionar_pasta_destino)
+button_selecionar_pasta_destino = Button(janela, text="Select Folder", font=("KelloggsSansMedium", 10,"bold"), command=selecionar_pasta_destino)
 button_selecionar_pasta_destino.pack(pady=5)
 
-button_gerar_qrcode = Button(janela, text="Gerar QR Codes", font=("KelloggsSansMedium", 10,"bold"), command=gerar_qrcode, width=20)
+button_gerar_qrcode = Button(janela, text="Generate QR Codes", font=("KelloggsSansMedium", 10,"bold"), command=gerar_qrcode, width=20)
 button_gerar_qrcode.pack(pady=(30,10))
 
-label_assinatura = Label(janela, text="Desenvolvido por: SLUK06", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 9,"bold"))
+label_assinatura = Label(janela, text="Dev by: SLUK06", bg="#D31145", fg="#FFFFFF", font=("KelloggsSansMedium", 9,"bold"))
 label_assinatura.pack()
 
 janela.mainloop()
